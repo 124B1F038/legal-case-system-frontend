b6c425f19068b16fb62e3c3fe89e970490643057
124B1F038 <divyagondhali2007@gmail.com>
2026-03-11 10:39:40 +0530

    changed backend url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://legal-case-system-frontend-3.onrender.com";
 const API_URL = `${BASE_URL}/api`;
 
 async function checkResponse(response) {
